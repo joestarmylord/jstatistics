98ebf9c1e696c8b915063cc05ebe0fad59ab8b1c
= <=>
2025-05-05 11:55:19 -0300

    fazer desafio

--- src/App.java
+++ src/App.java
@@ -1,5 +1,70 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô Git!");
+        System.out.println("Aula 3 - Switch e Loops");
+        // O switch é uma estrutura de seleção multipla
+        // um if mais organizado
+        // escolha
+            // caso
+        // indicado para resolver listas ou constantes
+            // exemplo: menu
+                // opção 1
+                // opção 2
+        //int op = Integer.parseInt(JOptionPane.showInputDialog("Digite a opção: \n1. Estatística \n2. Probalididade \n3. Modelos Probabilísticos \n4. Sair"));
+
+        //if (op != 4) { // != significa diferente
+        //    switch (op) {
+        //        case 1:JOptionPane.showMessageDialog(null, "Estatística Descritiva.");
+        //            break;
+            
+        //        case 2:JOptionPane.showMessageDialog(null, "Probabilidade.");
+        //            break;
+                
+        //        case 3:JOptionPane.showMessageDialog(null, "Modelos Probabilísticos.");
+        //            break;
+    
+        //        default:JOptionPane.showMessageDialog(null, "Opção inválida!");
+        //            break;
+        //    }
+        //} else {
+        //    JOptionPane.showMessageDialog(null, "Sistema Encerrado.");
+        //}
+
+        // como funciona os loops
+        // while - teste no inicio
+        // for - while compacto
+        // do while - teste no final
+        // 1. conceito: contador/inicio = 1
+        // 2. conceito: condição <=5
+        // 3. conceito: incremento/decremento 1=1+1
+
+        //int contador = 1;
+        //while (contador<3) {
+        //    JOptionPane.showMessageDialog(null, "Viva o SENAI!");
+        //    contador = contador+1;
+        //}
+
+        //int i = 1;
+        //while (i<=5) {
+        //    System.out.println(i);
+        //    i++;
+        //}
+
+        //for(int cont=1;cont<=10;cont++) {
+        //    System.out.println(cont);
+        //} // inicio;condicao;incremento
+
+        //String sair;
+        //do {
+
+            //System.out.println("Sair: s ou n?");
+        //    sair = JOptionPane.showInputDialog(null, "Sair: s ou n?");
+
+        //} while (sair.equalsIgnoreCase("sim")); {
+            
+        // }
+        //}
+
     }
 }
