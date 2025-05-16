86b265ccb5f37801a0be97302ff58d75d3ac698a
joestarmylord <gustavocosta.gglsc@gmail.com>
2025-05-16 10:28:15 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -5,17 +5,17 @@ import javax.swing.JOptionPane;
 public class App {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Alô, Jardim!", "Jardim", JOptionPane.OK_OPTION);
+        JOptionPane.showMessageDialog(null, "Alô, Jardim!", "Jardim do Ébano", JOptionPane.INFORMATION_MESSAGE);
 
-        String nome = JOptionPane.showInputDialog(null, "Preencha o campo abaixo com seu nome completo.", "Jardim", JOptionPane.OK_OPTION);
-        String endereco = JOptionPane.showInputDialog(null, "Preencha o campo abaixo com seu endereço (letras e números).", "Jardim", JOptionPane.OK_OPTION);
-        String telefone = JOptionPane.showInputDialog(null, "Preencha o campo abaixo com seu número de telefone (apenas números).", "Jardim", JOptionPane.OK_OPTION);
+        String nome = JOptionPane.showInputDialog(null, "Preencha o campo abaixo com seu nome completo.", "Jardim do Ébano", JOptionPane.INFORMATION_MESSAGE);
+        String endereco = JOptionPane.showInputDialog(null, "Preencha o campo abaixo com seu endereço (letras e números).", "Jardim do Ébano", JOptionPane.INFORMATION_MESSAGE);
+        String telefone = JOptionPane.showInputDialog(null, "Preencha o campo abaixo com seu número de telefone (apenas números).", "Jardim do Ébano", JOptionPane.INFORMATION_MESSAGE);
 
-        double largura = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe a largura do jardim em metros:", "Jardim", JOptionPane.OK_OPTION));
-        double comprimento = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o comprimento do jardim em metros:", "Jardim", JOptionPane.OK_OPTION));
+        double largura = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe a largura do jardim em metros:", "Jardim do Ébano", JOptionPane.INFORMATION_MESSAGE));
+        double comprimento = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o comprimento do jardim em metros:", "Jardim do Ébano", JOptionPane.INFORMATION_MESSAGE));
         double area = largura * comprimento;
 
-        JOptionPane.showMessageDialog(null, "Dados salvos!", "Jardim", JOptionPane.OK_OPTION);
+        JOptionPane.showMessageDialog(null, "Dados salvos!", "Jardim do Ébano", JOptionPane.INFORMATION_MESSAGE);
 
         try {
             FileWriter writer = new FileWriter("dadoscliente.txt");
@@ -23,7 +23,7 @@ public class App {
             writer.write(nome + ", " + endereco + ", " + telefone + ", " + area + "\n");
             writer.close();
         } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "Erro ao salvar o arquivo", "Jardim", JOptionPane.OK_OPTION);
+            JOptionPane.showMessageDialog(null, "Erro ao salvar o arquivo", "Jardim do Ébano", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 }
